0979a3ba9b1ac13e4a56f2ec47284d3bbf99b65e
dylan dray <dylan.dray@edu.devinci.fr>
2026-02-18 15:50:46 +0100

    resolved the error 500, refreshed the comments

--- Creating_Model.py
+++ Creating_Model.py
@@ -1,10 +1,10 @@
 from ucimlrepo import fetch_ucirepo 
 import pandas as pd
-from sklearn.preprocessing import OrdinalEncoder #transformers de scikit learn (low, high -> chiffres)
+from sklearn.preprocessing import OrdinalEncoder #transformers of scikit learn (L,H,m encoded)
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-import joblib #sauvegarde le modèle
+import joblib #save the model
 
 # fetch dataset from the api directly, just to save memory on my computer
 dataset = fetch_ucirepo(id=601) 
@@ -16,17 +16,17 @@ Y = dataset.data.targets["Machine failure"]
 print(X.head())
 print(Y.head())
 
-#Encodage, L < M < H
+#Encoding, L < M < H
 encoder = OrdinalEncoder(categories=[["L","M","H"]])
 X[["Type"]] = encoder.fit_transform(X[["Type"]])
 
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.2, random_state= 23) #le modèle utilise 1-0.2 données pour apprendre la logique, se train, et se testera sur les 20% restants
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.2, random_state= 23) #the model uses 1-0.2 lines of data to learn the logic and to train. It will then pass the "exam" on the 20% remaining
 #_train : training phase, _test: testing phase
 model = RandomForestClassifier(n_estimators= 100, random_state=23)
-model.fit(X_train, Y_train) #il analyse les lignes d'entrainements et tire des conclusions, trouve des liens
+model.fit(X_train, Y_train) #Find links and draw conclusions for the training phase
 
-Y_pred = model.predict(X_test) #maintenant il prédit les lignes restantes
-print(classification_report(Y_test, Y_pred)) #fait un rapport 
+Y_pred = model.predict(X_test) #now predicts the remaining lines
+print(classification_report(Y_test, Y_pred)) #do a report 
 
-joblib.dump(model, "maintenance_model.joblib")
+joblib.dump(model, "maintenance_model.joblib") #save the joblib files in order to use them in main.py
 joblib.dump(encoder, "type_encoder.joblib")
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ encoder = joblib.load("type_encoder.joblib")
 
 #types that the user must send
 class DataMachine(BaseModel):
-    Type: str #type was encoded
+    Type: str #type will be encoded later
     Air_temperature: float
     Process_temperature: float
     Rotational_speed: int
@@ -24,25 +24,25 @@ def home():
 
 @app.post("/predict")
 def predict(data: DataMachine):
-    # a. On transforme le JSON reçu en DataFrame
-    df = pd.DataFrame([data.model_dump()])
-    
-    # b. On renomme les colonnes pour que l'encodeur et le modèle s'y retrouvent
-    # On remet les noms exacts du dataset original
-    df.columns = [
-        'Type', 'Air temperature [K]', 'Process temperature [K]',
-        'Rotational speed [rpm]', 'Torque [Nm]', 'Tool wear [min]'
-    ]
-    
-    # c. On traduit le 'Type' (L/M/H) grâce à l'encodeur chargé
-    df[['Type']] = encoder.transform(df[['Type']])
-    
-    # d. On lance la prédiction
-    prediction = model.predict(df)[0]
-    probability = model.predict_proba(df)[0][1]
-    
-    return {
-        "prediction": int(prediction),
-        "probability": round(float(probability), 2),
-        "status": "DANGER: Maintenance requise" if prediction == 1 else "OK"
-    }
+    try:
+        df = pd.DataFrame([data.model_dump()])
+        df.columns = [
+            'Type', 
+            'Air temperature', 
+            'Process temperature',
+            'Rotational speed', 
+            'Torque', 
+            'Tool wear'
+        ]
+        df[['Type']] = encoder.transform(df[['Type']])
+        
+        prediction = model.predict(df)[0]
+        prob = model.predict_proba(df)[0][1]
+        
+        return {
+            "prediction": int(prediction),
+            "probability": f"{prob:.2%}",
+            "status": "DANGER" if prediction == 1 else "OK"
+        }
+    except Exception as e:
+        return {"error": str(e), "type_of_error": str(type(e))}
